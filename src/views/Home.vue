4c33dc4920e8e06d4a32287e6073f42c24765e95
davidgrldo <davidgrldo123@gmail.com>
2020-05-03 02:10:59 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderDavalyze />
+    <HeroDavalyze />
+    <WomenDavalyze />
+    <InstaDavalyze />
+    <PartnerDavalyze />
+    <FooterDavalyze />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import HeaderDavalyze from "@/components/HeaderDavalyze";
+import HeroDavalyze from "@/components/HeroDavalyze";
+import WomenDavalyze from "@/components/WomenDavalyze";
+import InstaDavalyze from "@/components/InstaDavalyze";
+import PartnerDavalyze from "@/components/PartnerDavalyze";
+import FooterDavalyze from "@/components/FooterDavalyze";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    HeaderDavalyze,
+    HeroDavalyze,
+    WomenDavalyze,
+    InstaDavalyze,
+    PartnerDavalyze,
+    FooterDavalyze
+  }
+};
+</script>
